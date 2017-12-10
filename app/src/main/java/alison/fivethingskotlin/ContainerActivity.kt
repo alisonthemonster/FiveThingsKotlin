d1427dedd0594cf122be23381e9398b1cc971813
alisonthemonster <alison.wyllie@gmail.com>
2017-12-10 14:45:40 -0800

    Add skeleton for navigationView

--- app/src/main/java/alison/fivethingskotlin/ContainerActivity.kt
+++ app/src/main/java/alison/fivethingskotlin/ContainerActivity.kt
@@ -2,11 +2,73 @@ package alison.fivethingskotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
+
 
 class ContainerActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
+
+        setUpNavigationDrawer()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpNavigationDrawer() {
+        //TODO come back and replace with view binding?
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val drawerToggle = ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.five_things_item -> {
+                    true
+                }
+                R.id.analytics_item -> {
+                    true
+                }
+                R.id.templates_item -> {
+                    true
+                }
+                R.id.settings_item -> {
+                    true
+                }
+                R.id.logout_item -> {
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
     }
 }
