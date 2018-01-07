07c8c82d0eba9db0868c1544857ca4607a3c8067
alisonthemonster <alison.wyllie@gmail.com>
2018-01-07 14:56:12 -0800

    Start work on recycler view

--- app/src/main/java/alison/fivethingskotlin/Fragments/DesignsFragment.kt
+++ app/src/main/java/alison/fivethingskotlin/Fragments/DesignsFragment.kt
@@ -1,35 +1,25 @@
 package alison.fivethingskotlin.Fragments
 
 import alison.fivethingskotlin.R
-import android.net.Uri
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.firebase.ui.storage.images.FirebaseImageLoader
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [DesignsFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [DesignsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class DesignsFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments.getString(ARG_PARAM1)
-            mParam2 = arguments.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -38,56 +28,82 @@ class DesignsFragment : Fragment() {
         return inflater!!.inflate(R.layout.designs_fragment, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
+    override fun onStart() {
+        super.onStart()
+        loadImages()
+
+    }
+
+    private fun loadImages() {
+        val storage = FirebaseStorage.getInstance()
+        val coverImages = storage.reference.child("CoverDesigns")
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.imagegallery)
+        recyclerView?.setHasFixedSize(true)
+
+        val layoutManager = GridLayoutManager(context, 2)
+        recyclerView?.layoutManager = layoutManager
+        val imageNames = getImageNames()
+        val adapter = GalleryAdapter(context, getImageNames())
+        recyclerView?.adapter = adapter
+    }
+
+    private fun getImageNames(): List<String> {
+        return listOf(
+                "AprJuneFloral.png",
+                "cover2017black.png",
+                "Diagonal2017.png",
+                "Floral2017.png",
+                "JanMarchFloral.jpg",
+                "JanMarchLetters.png",
+                "JulyAugSepFloral.png")
+    }
+
+}
+
+class GalleryAdapter: RecyclerView.Adapter<GalleryAdapter.MyViewHolder>() {
+
+    private lateinit var imageNames: Array<String>
+    private lateinit var mContext: Context
+    private lateinit var storage: FirebaseStorage
+
+    fun GalleryAdapter(context: Context, imagesList: Array<SpacePhoto>) {
+        mContext = context
+        imageNames = imagesList
+        storage = FirebaseStorage.getInstance()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryAdapter.MyViewHolder {
+
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val photoView = inflater.inflate(R.layout.item_photo, parent, false)
+        val viewHolder = GalleryAdapter.MyViewHolder(photoView)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val image = imageNames[position]
+        val imageView = holder.itemView as ImageView
+        val storageReference = storage.reference.child("CoverDesigns").child(image)
+
+        Glide.with(mContext)
+                .using(FirebaseImageLoader())
+                .load(storageReference)
+                .into(imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return imageNames.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var mPhotoImageView: ImageView
+
+        fun MyViewHolder(itemView: View) {
+
+            mPhotoImageView = itemView.findViewById<View>(R.id.iv_photo) as ImageView
+            itemView.setOnClickListener(this)
         }
     }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-
-         * @param param1 Parameter 1.
-         * *
-         * @param param2 Parameter 2.
-         * *
-         * @return A new instance of fragment DesignsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): DesignsFragment {
-            val fragment = DesignsFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
-        }
-    }
-}// Required empty public constructor
+}
