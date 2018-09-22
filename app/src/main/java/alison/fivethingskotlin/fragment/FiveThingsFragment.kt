43d7140555c802a6a8f71343cc464bbc22b715b8
alisonthemonster <alison.wyllie@gmail.com>
2018-09-22 10:50:30 -0700

    Enable editing mode with long press

--- app/src/main/java/alison/fivethingskotlin/fragment/FiveThingsFragment.kt
+++ app/src/main/java/alison/fivethingskotlin/fragment/FiveThingsFragment.kt
@@ -11,24 +11,26 @@ import alison.fivethingskotlin.util.*
 import alison.fivethingskotlin.viewmodel.FiveThingsViewModel
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import com.crashlytics.android.Crashlytics
 import com.github.sundeepk.compactcalendarview.CompactCalendarView
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.Observables
 import kotlinx.android.synthetic.main.fragment_five_things.*
 import net.openid.appauth.AuthorizationService
 import java.lang.Exception
 import java.util.*
 import java.util.concurrent.TimeUnit
-import com.crashlytics.android.Crashlytics
 
 
 class FiveThingsFragment : Fragment() {
@@ -61,6 +63,7 @@ class FiveThingsFragment : Fragment() {
         binding = FragmentFiveThingsBinding.inflate(inflater, container, false)
         binding.loading = true
         binding.saving = false
+        binding.inEditMode = false
 
         viewModel = ViewModelProviders.of(this).get(FiveThingsViewModel::class.java)
 
@@ -78,7 +81,6 @@ class FiveThingsFragment : Fragment() {
 
         return binding.root
     }
-
 
     override fun onStart() {
         super.onStart()
@@ -163,6 +165,9 @@ class FiveThingsFragment : Fragment() {
         val authorizationService = AuthorizationService(context!!)
         val authState = restoreAuthState(context!!)
 
+        if (authState == null) {
+            handleErrorState("Log in failed", context!!)
+        }
         authState?.performActionWithFreshTokens(authorizationService) { accessToken, idToken, ex ->
             if (ex != null) {
                 binding.loading = false
@@ -179,7 +184,8 @@ class FiveThingsFragment : Fragment() {
                             Thing(getDatabaseStyleDate(currentDate), twoEvent.editable().toString(), 2),
                             Thing(getDatabaseStyleDate(currentDate), threeEvent.editable().toString(), 3),
                             Thing(getDatabaseStyleDate(currentDate), fourEvent.editable().toString(), 4),
-                            Thing(getDatabaseStyleDate(currentDate), fiveEvent.editable().toString(), 5)) }
+                            Thing(getDatabaseStyleDate(currentDate), fiveEvent.editable().toString(), 5))
+                }
                         .skip(1) //skip the edit text binding
                         .debounce(1000, TimeUnit.MILLISECONDS)
                         .observeOn(AndroidSchedulers.mainThread())
@@ -196,6 +202,35 @@ class FiveThingsFragment : Fragment() {
             }
         }
 
+        //TODO have some sort of message that user has to long press to edit a day
+        binding.one.setOnLongClickListener { toggleEditMode(binding.one) }
+        binding.one.setOnClickListener { editTextClick() }
+        binding.two.setOnLongClickListener { toggleEditMode(binding.two) }
+        binding.two.setOnClickListener { editTextClick() }
+        binding.three.setOnLongClickListener { toggleEditMode(binding.three) }
+        binding.three.setOnClickListener { editTextClick() }
+        binding.four.setOnLongClickListener { toggleEditMode(binding.four) }
+        binding.four.setOnClickListener { editTextClick() }
+        binding.five.setOnLongClickListener { toggleEditMode(binding.five) }
+        binding.five.setOnClickListener { editTextClick() }
+    }
+
+    private fun toggleEditMode(editText: EditText): Boolean {
+        binding.inEditMode = !(binding.inEditMode)!!
+
+        val imm = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (binding.inEditMode == false) {
+            imm.hideSoftInputFromWindow(editText.windowToken, 0)
+        } else {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        }
+        makeToast(context!!, "Switching ${if (binding.inEditMode!!) "to" else "from"} edit mode")
+        return true
+    }
+
+    private fun editTextClick() {
+        if (!binding.inEditMode!!)
+            makeToast(context!!, "Long press to edit a day")
     }
 
     private fun getFiveThings() {
